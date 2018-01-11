660bb7d7f5ed7e8db5e928730cb64b649dfcd9e1
Félix Queiruga Balado <f.queiruga.balado@accenture.com>
2018-01-11 16:15:49 +0100

    Add prettier and eslint

---
+++ src/components/StartPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <router-link to="/hello-world">Sign in</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StartPage"
+};
+</script>
+
+<style scoped>
+.container {
+  text-align: center;
+  margin: auto;
+}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import StartPage from "@/components/StartPage";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
+      path: "/",
+      name: "Start Page",
+      component: StartPage
+    },
+    {
+      path: "/hello-world",
+      name: "Hello World",
       component: HelloWorld
     }
   ]
-})
+});
